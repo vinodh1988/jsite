05404adf4c25825b8e8a840a1bda69329893b90b
vinodh1988 <k.c.vinodh@hotmail.com>
2021-05-18 17:15:10 +0530

    first-commit

---
+++ index.js
@@ -0,0 +1,32 @@
+
+temp=setInterval(function(){
+   var hours = Number(document.getElementById("hours").innerText);
+   var minutes = Number(document.getElementById("minutes").innerText);
+   var seconds = Number(document.getElementById("seconds").innerText);
+   if(seconds==00){
+    seconds=59;
+    if(minutes==0){
+       minutes=59;
+       if(hours==0)
+       clearfun();
+       else
+       hours-=1;
+    }
+    else
+    minutes-=1;
+   }
+   else{
+       seconds-=1;
+   }
+  
+   
+   document.getElementById("hours").innerText=hours;
+   document.getElementById("minutes").innerText=minutes;
+   document.getElementById("seconds").innerText=seconds;
+   
+  
+},1000);
+
+function clearfun(){
+    clearInterval(temp);
+}
